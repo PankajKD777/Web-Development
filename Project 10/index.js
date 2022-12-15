1c4d0a43b92de412a458d1508dc44af238310535
Pankaj Kumar Das <109813321+PankajKD777@users.noreply.github.com>
2022-12-15 20:28:09 +0530

    Add files via upload
    Adding new projects

---
+++ Project 10/index.js
@@ -0,0 +1,10 @@
+function calculateLoan(){
+    const amount = document.getElementById("amount").value
+    const interest_rate = document.getElementById("interest_rate").value
+    const months = document.getElementById("months").value
+    const interest =(amount *(interest_rate* .1))/ months
+
+    const payment = (amount/months+interest).toFixed(2)
+
+    document.getElementById("payment").innerHTML = 'monthly payment: $(payment)'
+}
